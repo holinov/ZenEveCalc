66b7145540a333eafd2972c5454068b2c716917a
holinov <holinov@netvoxlab.ru>
2013-07-01 10:02:28 +0400

    Доработки интерфейса

--- Zen.EveCalc/Controls/BlueprintsList.xaml.cs
+++ Zen.EveCalc/Controls/BlueprintsList.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -169,6 +170,21 @@ namespace Zen.EveCalc.Controls
         public void Show()
         {
             Load();
+        }
+    }
+
+    public class FavoriteBorderConverter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            var val = (bool) value;
+            return val ? new Thickness(5, 0, 0, 0) : new Thickness(0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 
